505346b485885e263a6454f2279f3ef614953450
kulovvoj <vojtech.kulovany@epptec.eu>
2021-06-21 13:44:42 +0200

    Added test sources

---
+++ test/eu/epptec/analyticGeometry/shapes/elementary/PointTest.java
@@ -0,0 +1,29 @@
+package eu.epptec.analyticGeometry.shapes.elementary;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void move() {
+
+    }
+
+    @Test
+    void rotate() {
+    }
+
+    @Test
+    void testRotate() {
+    }
+
+    @Test
+    void getCenter() {
+    }
+
+    @Test
+    void getIntersectingPoints() {
+    }
+}
